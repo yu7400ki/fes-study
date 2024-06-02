daa8bbf2c8e084bcb1d65fbc05407736924cd4ca
yu7400ki <yu7400ki@gmail.com>
2024-06-02 21:26:23 +0900

    typography

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,8 +1,15 @@
 import type { MDXComponents } from "mdx/types";
+import { Text } from "@/components/ui";
 
 // biome-ignore lint/style/useNamingConvention:
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children }) => <Text size="4xl">{children}</Text>,
+    h2: ({ children }) => <Text size="3xl">{children}</Text>,
+    h3: ({ children }) => <Text size="2xl">{children}</Text>,
+    h4: ({ children }) => <Text size="xl">{children}</Text>,
+    h5: ({ children }) => <Text size="md">{children}</Text>,
+    h6: ({ children }) => <Text size="sm">{children}</Text>,
     ...components,
   };
 }

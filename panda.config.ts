36bb9802619d439dd1582fed8e9baadc294d340a
yu7400ki <yu7400ki@gmail.com>
2024-06-02 17:26:25 +0900

    park-ui

--- panda.config.ts
+++ panda.config.ts
@@ -1,20 +1,17 @@
 import { defineConfig } from "@pandacss/dev";
+import { createPreset } from "@park-ui/panda-preset";
 
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
-
-  // Where to look for your css declarations
+  presets: [
+    "@pandacss/preset-base",
+    createPreset({
+      accentColor: "neutral",
+      grayColor: "neutral",
+      borderRadius: "md",
+    }),
+  ],
   include: ["./src/**/*.{js,jsx,ts,tsx}"],
-
-  // Files to exclude
-  exclude: [],
-
-  // Useful for theme customization
-  theme: {
-    extend: {},
-  },
-
-  // The output directory for your css system
+  jsxFramework: "react",
   outdir: "styled-system",
 });

59eebeafec3646eb40894644b5486c0b89632e4f
yu7400ki <yu7400ki@gmail.com>
2024-06-06 15:20:34 +0900

    collapse

--- src/app/_components/sidebar.tsx
+++ src/app/_components/sidebar.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { MoonIcon, SunIcon } from "lucide-react";
+import { MoonIcon, PanelLeftClose, PanelLeftOpen, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import { css, cx } from "styled-system/css";
 import { iconButton } from "styled-system/recipes";
 
@@ -19,19 +20,28 @@ type Props = {
 export default function Sidebar({ data }: Props) {
   const pathname = usePathname();
   const { theme = "light", setTheme } = useTheme();
+  const [open, setOpen] = useState(true);
 
   return (
     <aside
-      className={css({
-        top: 12,
-        mt: 12,
-        position: "sticky",
-        fontSize: "sm",
-        width: 64,
-        placeSelf: "flex-start",
-        flexShrink: 0,
-        px: 4,
-      })}
+      className={cx(
+        css({
+          top: 12,
+          mt: 12,
+          position: "sticky",
+          fontSize: "sm",
+          width: 64,
+          placeSelf: "flex-start",
+          flexShrink: 0,
+          px: 4,
+          transition: "width 0.2s",
+          "&[data-open=false]": {
+            width: 16,
+          },
+        }),
+        "group",
+      )}
+      data-open={open}
     >
       <nav
         className={css({
@@ -40,6 +50,11 @@ export default function Sidebar({ data }: Props) {
           gap: 6,
           pb: 6,
           height: "calc(100vh - {spacing.12} - {spacing.14})",
+          overflowX: "hidden",
+          ".group[data-open=false] &": {
+            height: "calc(100vh - {spacing.12} - {spacing.20})",
+            width: 0,
+          },
         })}
       >
         {data.map((section) => (
@@ -57,6 +72,7 @@ export default function Sidebar({ data }: Props) {
                   <Link
                     href={page.path}
                     className={css({
+                      whiteSpace: "nowrap",
                       color: {
                         base: "fg.muted",
                         _currentPage: "accent.default",
@@ -83,6 +99,13 @@ export default function Sidebar({ data }: Props) {
           height: 14,
           display: "flex",
           alignItems: "center",
+          justifyContent: "space-between",
+          ".group[data-open=false] &": {
+            borderTop: "none",
+            height: 20,
+            flexDirection: "column",
+            justifyContent: "center",
+          },
         })}
       >
         <button
@@ -91,6 +114,13 @@ export default function Sidebar({ data }: Props) {
           onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         >
           {theme === "light" ? <SunIcon /> : <MoonIcon />}
+        </button>
+        <button
+          type="button"
+          className={cx(iconButton({ size: "xs", variant: "ghost" }), css({ color: "fg.muted" }))}
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <PanelLeftClose /> : <PanelLeftOpen />}
         </button>
       </div>
     </aside>

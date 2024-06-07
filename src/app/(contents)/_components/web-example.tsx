79e2ae6a7c42020b22a82edc94626fe90fdb8e66
yu7400ki <yu7400ki@gmail.com>
2024-06-07 23:20:45 +0900

    unescape style

--- src/app/(contents)/_components/web-example.tsx
+++ src/app/(contents)/_components/web-example.tsx
@@ -5,13 +5,25 @@ type Props = {
   children: React.ReactNode;
 };
 
+function unescapeHtml(html: string): string {
+  return html
+    .replace(/&amp;/g, "&")
+    .replace(/&apos;/g, "'")
+    .replace(/&quot;/g, '"')
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">");
+}
+
 export default async function WebExample({ children }: Props) {
   const ReactDomServer = (await import("react-dom/server")).default;
   const html = ReactDomServer.renderToStaticMarkup(children);
+  const unescapedHtml = html.replace(/<style>(.*?)<\/style>/s, (_, p1) => {
+    return `<style>${unescapeHtml(p1)}</style>`;
+  });
 
   return (
     <Box border="1px solid" borderColor="border.default" borderRadius="md" p={4}>
-      <ShadowDom html={html} />
+      <ShadowDom html={unescapedHtml} />
     </Box>
   );
 }
